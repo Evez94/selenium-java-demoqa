fdc3e615b2e6f7d09a0349a4685ebf33221c4567
Evez Aslanov <evez.aslanov.94@gmail.com>
2026-08-22 01:28:53 +0400

    fixed basetest for docker

--- src/test/java/com/demoqa/tests/javascript/JavaScriptTest.java
+++ src/test/java/com/demoqa/tests/javascript/JavaScriptTest.java
@@ -1,6 +1,7 @@
 package com.demoqa.tests.javascript;
 
 import com.demoqa.base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JavaScriptTest  extends BaseTest {
@@ -8,5 +9,10 @@ public class JavaScriptTest  extends BaseTest {
     @Test
     public void testScrollingToElement(){
         homePage.goToForms();
+        System.out.println("LOG: Form kartına uğurla klik olundu.");
+
+        // Yoxlama (Assertion)
+        Assert.assertTrue(driver.getCurrentUrl().contains("forms"), "Form səhifəsinə keçid alınmadı!");
+        System.out.println("LOG: Form səhifəsinin URL-i doğrulandı.");
     }
 }
