7336f3315665a0d072d3424290fef451bccb9409
Evez Aslanov <evez.aslanov.94@gmail.com>
2026-08-22 01:21:57 +0400

    fixed basetest for docker

--- src/test/java/com/demoqa/base/BaseTest.java
+++ src/test/java/com/demoqa/base/BaseTest.java
@@ -1,42 +1,43 @@
-package com.demoqa.base;
-
-import com.demoqa.pages.base.BasePage;
-import com.demoqa.pages.HomePage;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-
-import static com.demoqa.pages.base.BasePage.delay;
-import static utilities.Utility.setUtilityDriver;
-
-public class BaseTest {
-    // Variable public yox, protected olmalıdır ki, miras alan (extends) klaslar oxuya bilsin
-    protected WebDriver driver;
-
-    protected BasePage basePage;
-    protected HomePage homePage;
-    private String DEMOQA_URL = "https://demoqa.com";
-
-
-    @BeforeClass
-    public void setUp(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-    }
-
-    @BeforeMethod
-    public void loadApplication(){
-        driver.get(DEMOQA_URL);
-        basePage = new BasePage();
-        basePage.setDriver(driver);
-        setUtilityDriver();
-        homePage = new HomePage();
-    }
-
-
+//package com.demoqa.base;
+//
+//import com.demoqa.pages.base.BasePage;
+//import com.demoqa.pages.HomePage;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeMethod;
+//
+//import static com.demoqa.pages.base.BasePage.delay;
+//import static utilities.Utility.setUtilityDriver;
+//
+//public class BaseTest {
+//    // Variable public yox, protected olmalıdır ki, miras alan (extends) klaslar oxuya bilsin
+//    protected WebDriver driver;
+//
+//    protected BasePage basePage;
+//    protected HomePage homePage;
+//    private String DEMOQA_URL = "https://demoqa.com";
+//
+//
+//    @BeforeClass
+//    public void setUp(){
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//
+//    }
+//
+//    @BeforeMethod
+//    public void loadApplication(){
+//        driver.get(DEMOQA_URL);
+//        basePage = new BasePage();
+//        basePage.setDriver(driver);
+//        setUtilityDriver();
+//        homePage = new HomePage();
+//    }
+//
+//
 //    @BeforeMethod
 //    public void setUp() {
 //        ChromeOptions options = new ChromeOptions();
@@ -48,8 +49,61 @@ public class BaseTest {
 //        driver = new ChromeDriver(options);
 //        driver.get("https://demoqa.com/text-box");
 //    }
+//
+//    @AfterMethod
+//    public void tearDown() {
+//        if (driver != null) {
+//            delay(3000);
+//            driver.quit();
+//        }
+//    }
+//}
 
-    @AfterMethod
+
+package com.demoqa.base;
+
+import com.demoqa.pages.base.BasePage;
+import com.demoqa.pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import static com.demoqa.pages.base.BasePage.delay;
+import static utilities.Utility.setUtilityDriver;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected BasePage basePage;
+    protected HomePage homePage;
+    private String DEMOQA_URL = "https://demoqa.com";
+
+    @BeforeClass
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @BeforeMethod
+    public void loadApplication() {
+        driver.get(DEMOQA_URL);
+        basePage = new BasePage();
+        basePage.setDriver(driver);
+        setUtilityDriver();
+        homePage = new HomePage();
+    }
+
+    @AfterClass
     public void tearDown() {
         if (driver != null) {
             delay(3000);
