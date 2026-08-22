a05dba2b17d411ae91e45249db916ff969a1bda3
Evez Aslanov <evez.aslanov.94@gmail.com>
2026-08-22 23:31:28 +0400

    FormsPage changing practice form locator

--- src/test/java/com/demoqa/base/BaseTest.java
+++ src/test/java/com/demoqa/base/BaseTest.java
@@ -28,7 +28,7 @@ public class BaseTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080"); // maximize() əvəzinə bu kifayətdir
+        options.addArguments("--window-size=1490,1080");
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
